1237611c68420c9d9af3b4af33cfe2191d946d11
Fast Bitcoin <fastbitcoin@techie.com>
2017-12-29 14:56:40 +0800

    fix checkpoints

--- libraries/blockchain/include/fbtc/blockchain/checkpoints.hpp
+++ libraries/blockchain/include/fbtc/blockchain/checkpoints.hpp
@@ -5,7 +5,7 @@ namespace fbtc { namespace blockchain {
 
 static std::map<uint32_t, fbtc::blockchain::block_id_type> CHECKPOINT_BLOCKS
 {
-	{ 700, fbtc::blockchain::block_id_type("1b19fca0aa74db6ee03ba23a2db88c0d2e7cb451") }
+	{ 700, fbtc::blockchain::block_id_type("e32f59446b3b4952ddef4d33b3adebdb311dbff7") }
 };
 
 // Initialized in load_checkpoints()
