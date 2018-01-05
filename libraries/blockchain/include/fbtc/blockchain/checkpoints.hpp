12f61699b0a858203ffd3031d8cf2199c842c4e0
fastbitcoin <34875903+fastbitcoin@users.noreply.github.com>
2018-01-05 09:21:49 +0800

    Update checkpoints.hpp

--- libraries/blockchain/include/fbtc/blockchain/checkpoints.hpp
+++ libraries/blockchain/include/fbtc/blockchain/checkpoints.hpp
@@ -5,7 +5,7 @@ namespace fbtc { namespace blockchain {
 
 static std::map<uint32_t, fbtc::blockchain::block_id_type> CHECKPOINT_BLOCKS
 {
-
+  { 700, fbtc::blockchain::block_id_type("e32f59446b3b4952ddef4d33b3adebdb311dbff7") }
 };
 
 // Initialized in load_checkpoints()
